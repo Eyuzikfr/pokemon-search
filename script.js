9ba47234cfce5c192d3daf8c9a3f656feacc4152
Eyuzikfr <eyuzikchhunzu2@gmail.com>
2025-03-28 16:26:46 +0545

    basic implementaion complete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
 const pokemonNameEl = document.getElementById("pokemon-name");
 const pokemonIdEl = document.getElementById("pokemon-id");
+const imgContainer = document.getElementById("img-container");
 const weightEl = document.getElementById("weight");
 const heightEl = document.getElementById("height");
 const typesEl = document.getElementById("types");
@@ -20,10 +21,29 @@ const fetchData = async (pokemon) => {
     const res = await fetch(endpoint);
     const data = await res.json();
     console.log(data);
+    showPokemon(data);
   } catch (err) {
     window.alert("Pokemon not found!");
     console.log(err);
   }
+};
+
+const showPokemon = (pokemonData) => {
+  pokemonNameEl.textContent = `${pokemonData.name.toUpperCase()}`;
+  pokemonIdEl.textContent = `${pokemonData.id}`;
+  imgContainer.innerHTML = `<img id="sprite" src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}'s image" />`;
+  weightEl.textContent = `Weight: ${pokemonData.weight}`;
+  heightEl.textContent = `Height: ${pokemonData.height}`;
+  console.log(pokemonData.types);
+  typesEl.innerHTML = pokemonData.types.map(
+    (slot) => `<span>${slot.type.name.toUpperCase()}</span>`
+  );
+  hpEl.textContent = `${pokemonData.stats[0].base_stat}`;
+  attackEl.textContent = `${pokemonData.stats[1].base_stat}`;
+  defenseEl.textContent = `${pokemonData.stats[2].base_stat}`;
+  spAtkEl.textContent = `${pokemonData.stats[3].base_stat}`;
+  spDefEl.textContent = `${pokemonData.stats[4].base_stat}`;
+  speedEl.textContent = `${pokemonData.stats[5].base_stat}`;
 };
 
 searchBtn.addEventListener("click", () => {
